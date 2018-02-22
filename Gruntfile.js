4264eb8fd0f44573e42c5b795e6b2a63fa2b2019
Johan Singainy-Moutiapin <johan.singainy-moutiapin@opendatasoft.com>
2018-02-22 14:37:31 +0100

    [ch11862] realign style header && change style sidebar / breadcrumb

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,10 +93,10 @@ module.exports = function(grunt) {
 		copy: {
 			docs: {
 				expand: true,
-				cwd: '../ods/img/',
+				cwd: 'assets/',
 				src: 'ODS_logo_widget_bleu.svg',
 				dest: 'assets/'
-			},
+            },
             libs: {
                 expand: true,
                 src: ['libs/**'],
@@ -119,7 +119,7 @@ module.exports = function(grunt) {
                     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
                 ],
                 template: 'src/templates/index.tmpl',
-				html5Mode: false,
+                html5Mode: false,
                 image: 'assets/ODS_logo_widget_blanc.svg',
 				title: "ODS Widgets",
 				bestMatch: true,
@@ -178,6 +178,6 @@ module.exports = function(grunt) {
 
     // Default task(s).
     grunt.registerTask('default', ['dist']);
-    grunt.registerTask('dist', ['clean', 'uglify:dist', 'less:dist', 'less:dev', 'concat', 'autoprefixer', 'copy:libs', 'ngdocs']);
+    grunt.registerTask('dist', ['clean', 'uglify:dist', 'less:dist', 'less:dev', 'concat', 'autoprefixer', 'copy', 'ngdocs']);
     grunt.registerTask('server', ['default', 'connect', 'watch']);
 };
