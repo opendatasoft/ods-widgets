6ed541f2a8c2075f9993bbc5098aac78989a8b57
Johan Singainy-Moutiapin <johan.singainy-moutiapin@opendatasoft.com>
2018-03-08 15:21:46 +0100

    [ch11862] adding switch item activ in help hub sidebar

--- src/templates/js/script.js
+++ src/templates/js/script.js
@@ -1,12 +1,5 @@
 //- ODS Custom Theme
-(function () {
-    document.body.addEventListener("click", function (event) {
-        if (event.target.className.includes('ng-binding')) {
-            toggle(menuSidebar, menuActive, btnMenu, headerBtnActive, 1);
-        }
-    });
-})();
-
+//- Sidebar Menu / Sidebar Help Hub
 var helpHubSidebar  = "ods__documentation-help-hub-sidebar"
     menuSidebar     = "ods__theme-sidebar"
     helpHubActive   = "help-hub-active"
@@ -52,3 +45,47 @@ function toggleMenu() {
         toggle(menuSidebar, menuActive, btnMenu, headerBtnActive, 1);
     }
 }
+
+//- Link in sidebar help hub (widgets / tutorial widgets)
+var itemActiv = "ods__documentation-header-nav-item-active"
+    widgets   = document.getElementById("link_widgets")
+    tutorial  = document.getElementById("link_tutorial");
+
+function toggleLinkActiv(a, b, c) {
+    if (a.className.includes(c)) console.log("Already have");
+    else {
+        a.classList.add(c);
+        b.parentElement.classList.remove(c);
+    }
+}
+
+function toggleLinkHelpHub(a) {
+    if (event.target.id == "link_widgets") {
+        toggleLinkActiv(event.target.parentElement, tutorial, itemActiv);
+    } else if (event.target.id == "link_tutorial") {
+        toggleLinkActiv(event.target.parentElement, widgets, itemActiv);
+    }
+}
+
+function assignHelpHubActiv() {
+    var url = window.location.href.split("#")[1];
+    
+    if (url == "/api") widgets.parentElement.classList.add(itemActiv);
+    else if (url == "/tutorial") tutorial.parentElement.classList.add(itemActiv);
+}
+
+(function () {
+
+    document.body.addEventListener("click", function (event) {
+        if (event.target.className.includes("ng-binding")) {
+            toggle(menuSidebar, menuActive, btnMenu, headerBtnActive, 1);
+        }
+        if (event.target.id.includes('link_widgets') ||
+            event.target.id.includes('link_tutorial')) {
+            toggleLinkHelpHub(event.target);
+        }
+    });
+
+    assignHelpHubActiv();
+
+})();
