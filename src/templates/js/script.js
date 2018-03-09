075f555d8d7f398c06174444b68384350c93da81
Johan Singainy-Moutiapin <johan.singainy-moutiapin@opendatasoft.com>
2018-03-09 16:20:23 +0100

    [ch11862] add meta for resolution mobile / update theme style on mobile / update classes namming

--- src/templates/js/script.js
+++ src/templates/js/script.js
@@ -1,12 +1,12 @@
 //- ODS Custom Theme
 //- Sidebar Menu / Sidebar Help Hub
-var helpHubSidebar  = "ods__documentation-help-hub-sidebar"
-    menuSidebar     = "ods__theme-sidebar"
-    helpHubActive   = "help-hub-active"
-    menuActive      = "menu-active"
-    btnHelpHub      = "help-hub-button"
-    btnMenu         = "nav-button"
-    headerBtnActive = "ods__documentation-header-btn-active";
+var helpHubSidebar  = "ods-helphub"
+    menuSidebar     = "ods-sidebar"
+    helpHubActive   = "ods-helphub--active"
+    menuActive      = "ods-sidebar--active"
+    btnHelpHub      = "helphub-button"
+    btnMenu         = "sidebar-button"
+    headerBtnActive = "ods-header__btn--active";
 
 function toggle (a, b, c, d, e) {
     if (e == 0) {
@@ -32,7 +32,19 @@ function toggleHelpHub() {
     }
 }
 
+//- Resize sidebar
+function resizeSidebar() {
+    var contentHeight = document.getElementById("content-principal").offsetHeight
+        sidebarHeight = document.getElementById('sidebar-nav').offsetHeight;
+
+    if (sidebarHeight < contentHeight) {
+        document.getElementById("sidebar-nav").style.height = contentHeight + "px";
+    }
+}
+
 function toggleMenu() {
+    resizeSidebar();
+
     var sidebar = document.getElementsByClassName(helpHubSidebar)[0].className
         menu    = document.getElementsByClassName(menuSidebar)[0].className;
 
@@ -46,14 +58,13 @@ function toggleMenu() {
     }
 }
 
-//- Link in sidebar help hub (widgets / tutorial widgets)
-var itemActiv = "ods__documentation-header-nav-item-active"
+//- Activ link in sidebar help hub (widgets / tutorial widgets)
+var itemActiv = "ods-header__nav-item--active"
     widgets   = document.getElementById("link_widgets")
     tutorial  = document.getElementById("link_tutorial");
 
 function toggleLinkActiv(a, b, c) {
-    if (a.className.includes(c)) console.log("Already have");
-    else {
+    if (!a.className.includes(c)) {
         a.classList.add(c);
         b.parentElement.classList.remove(c);
     }
@@ -74,7 +85,7 @@ function assignHelpHubActiv() {
     else if (url == "/tutorial") tutorial.parentElement.classList.add(itemActiv);
 }
 
-(function () {
+(function () { 
 
     document.body.addEventListener("click", function (event) {
         if (event.target.className.includes("ng-binding")) {
